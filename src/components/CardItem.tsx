2751041ff74f5fca2597ad34c9ece293cdd62767
brygmos <daniel-best@yandex.ru>
2023-03-16 12:22:48 +0500

    feat: card styling

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import cl from './styles/CardItem.module.css';
-import { faEye, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark, faEye, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 type State = {
   card: string;
@@ -34,16 +34,31 @@ class CardItem extends React.Component<Props, State> {
     return (
       <div className={cl.card}>
         <img className={cl.img} src={this.props.card.image} alt="card image" />
-        <h1>{this.props.card.title}</h1>
-        <p>
-          Author:
-          <span>{this.props.card.author?.firstname}</span>
-          <span> {this.props.card.author?.lastname}</span>
-        </p>
-        <FontAwesomeIcon icon={faEye} />
-        <span>{this.props.card.views}</span>
-        <FontAwesomeIcon icon={faThumbsUp} />
-        <span>{this.props.card.likes}</span>
+        <div className={cl.textContent}>
+          <div className={cl.header}>
+            <h1>{this.props.card.title}</h1>
+            <p>
+              Author:
+              <a href="#">
+                <span>{this.props.card.author?.firstname}</span>
+                <span> {this.props.card.author?.lastname}</span>
+              </a>
+            </p>
+          </div>
+          <div className={cl.btns}>
+            <div className={cl.like}>
+              <FontAwesomeIcon icon={faThumbsUp} />
+              <span>{this.props.card.likes}</span>
+            </div>
+            <div className={cl.bookmark}>
+              <FontAwesomeIcon icon={faBookmark} />
+            </div>
+          </div>
+          <div className={cl.views}>
+            <FontAwesomeIcon icon={faEye} />
+            <span>{this.props.card.views}</span>
+          </div>
+        </div>
       </div>
     );
   }
