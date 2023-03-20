af442750e734c8794e9e3d094ac60310a55830cc
brygmos <daniel-best@yandex.ru>
2023-03-20 19:59:41 +0500

    fix: add title in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { FC } from 'react';
 import Navbar from './Navbar';
 import cl from './styles/Header.module.css';
 
-const Header: FC = (): JSX.Element => {
-  return (
-    <header className={cl.header}>
-      <Navbar />
-    </header>
-  );
+type ComponentProps = {
+  children?: React.ReactNode;
 };
 
-export default Header;
+type ComponentState = {
+  title: string;
+};
+
+type updateTitle = () => void;
+
+export class Header extends React.Component<ComponentProps, ComponentState> {
+  constructor(props: ComponentProps) {
+    super(props);
+    this.state = { title: location.pathname };
+  }
+
+  updateTitle: updateTitle = (): void => {
+    this.setState({ title: location.pathname });
+  };
+
+  render(): React.ReactNode {
+    return (
+      <header className={cl.header}>
+        <h1 className="headerTitle" role="headerTitle">
+          {this.state.title === '/' ? 'Main' : this.state.title === '/about' ? 'About' : '/404'}
+        </h1>
+        <Navbar updateTitle={this.updateTitle} />
+      </header>
+    );
+  }
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import cl from './styles/Navbar.module.css';
 
-const Navbar: FC = (): JSX.Element => {
-  return (
-    <nav role={'navigation'} className={cl.navbar}>
-      <NavLink className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)} to="/">
-        Main
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)}
-        to="/about"
-      >
-        About
-      </NavLink>
-      <NavLink className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)} to="/404">
-        Error
-      </NavLink>
-    </nav>
-  );
+type ComponentProps = {
+  updateTitle: updateTitle;
 };
+
+type ComponentState = {
+  title: string;
+};
+
+type updateTitle = () => void;
+
+export class Navbar extends React.Component<ComponentProps, ComponentState> {
+  render(): JSX.Element {
+    return (
+      <nav
+        role={'navigation'}
+        className={cl.navbar}
+        onClick={() => {
+          this.props.updateTitle();
+        }}
+      >
+        <NavLink className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)} to="/">
+          Main
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? cl.navlink_active : cl.navlink)}
+          to="/404"
+        >
+          Error
+        </NavLink>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
