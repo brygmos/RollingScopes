3c0b094ea1715f21c7280e821d53d99f49b96507
brygmos <daniel-best@yandex.ru>
2023-03-25 16:19:59 +0500

    refactor: searchbar to hooks

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import cl from './styles/SeachBar.module.css';
 
-type State = {
-  finder: string;
+const SearchBar: FC = () => {
+  const [finder, setFinder] = useState(localStorage.getItem('finder') || '');
+
+  useEffect(() => {
+    const input: string = finder || '';
+    localStorage.setItem('finder', input);
+  }, [finder]);
+
+  return (
+    <div className={cl.container}>
+      <label htmlFor="searchbar" hidden={true}>
+        Search...
+      </label>
+      <input
+        id={'searchbar'}
+        className={cl.input}
+        value={finder}
+        placeholder={'Search...'}
+        type="text"
+        onChange={(e) => setFinder(e.currentTarget.value)}
+      />
+    </div>
+  );
 };
-
-class SearchBar extends React.Component {
-  state: State = {
-    finder: localStorage.getItem('finder') || '',
-  };
-
-  handleFinder = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const finderValue = e.currentTarget.value;
-    this.setState({ finder: finderValue });
-  };
-
-  componentWillUnmount(): void {
-    localStorage.setItem('finder', this.state.finder);
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className={cl.container}>
-        <label htmlFor="searchbar" hidden={true}>
-          Search...
-        </label>
-        <input
-          id={'searchbar'}
-          className={cl.input}
-          value={this.state.finder}
-          placeholder={'Search...'}
-          type="text"
-          onChange={(e) => this.handleFinder(e)}
-        />
-      </div>
-    );
-  }
-}
 
 export default SearchBar;
