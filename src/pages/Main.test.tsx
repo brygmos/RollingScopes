01f01318b022b8407fbb285e391f68ddc0b402b5
brygmos <daniel-best@yandex.ru>
2023-04-07 14:00:16 +0300

    feat: implement fetch mocking, add test

--- src/pages/Main.test.tsx
+++ src/pages/Main.test.tsx
@@ -37,6 +37,7 @@ describe('fetch', () => {
     const response = await fetch('https://rickandmortyapi.com/api/character');
     const data = await response.json();
     expect(data.results[0].id).toEqual(20);
+    expect(data.results[0].name).toEqual('Ants in my Eyes Johnson');
     // await expect(screen.findByText(/Ants in my Eyes Johnson/i)).toBeInTheDocument();
     screen.debug();
   });
